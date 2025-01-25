2d0fb6e3ae20a90f18bb50fe1c729138bd89c9d5
Ron Moerman <ron@crazyelectron.io>
2025-01-25 07:48:31 +0100

    Add registry login
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- cluster/apps/harbor-system/post-deploy-job/upload/harbor-update.sh
+++ cluster/apps/harbor-system/post-deploy-job/upload/harbor-update.sh
@@ -4,3 +4,9 @@ echo "Harbor Token:" $HARBOR_TOKEN
 echo "Harbor URL:" $HARBOR_URL
 echo "Harbor Namespace:" $HARBOR_NAMESPACE 
 echo "Harbor robot account:" $HARBOR_ACCOUNT
+echo "Harbor repositories file:" $HARBOR_REPOSITORIES
+
+dig $HARBOR_URL
+
+echo "Login to the harbor registry"
+echo $HARBOR_TOKEN | docker login $HARBOR_URL -u $HARBOR_ACCOUNT --password-stdin
