fe83984782d97484145a61c374cfdb8d9a65a444
Ron Moerman <ron@crazyelectron.io>
2025-01-25 15:45:50 +0100

    Add Mealie GitRepository
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- cluster/apps/harbor-system/post-deploy-job/upload/harbor-update.sh
+++ cluster/apps/harbor-system/post-deploy-job/upload/harbor-update.sh
@@ -6,7 +6,33 @@ echo "Harbor Namespace:" $HARBOR_NAMESPACE
 echo "Harbor robot account:" $HARBOR_ACCOUNT
 echo "Harbor repositories file:" $HARBOR_REPOSITORIES
 
-dig $HARBOR_URL
-
 echo "Login to the harbor registry"
-echo $HARBOR_TOKEN | docker login $HARBOR_URL -u $HARBOR_ACCOUNT --password-stdin
+#echo $HARBOR_TOKEN | docker login $HARBOR_URL -u $HARBOR_ACCOUNT --password-stdin
+
+
+# RESPONSE=$(curl "https://registry.moerman.online/api/v2.0/robots" \
+#   -H 'athorization: Basic YWRtaW46ZGlybUVmLXphZ3ZpMC10ZXpkdXA=' \
+#   -H 'content-type: application/json' \
+#   -d '{
+#        "count_limit": 10,
+#        "project_name": "library",
+#        "cve_whitelist": {
+#          "items": [
+#            {
+#              "cve_id": "string"
+#            }
+#          ],
+#          "project_id": 0,
+#          "id": 0,
+#          "expires_at": 0
+#        },
+#        "storage_limit": 10,
+#        "metadata": {
+#          "enable_content_trust": "true",
+#          "auto_scan": "false",
+#          "severity": "none",
+#          "reuse_sys_cve_whitelist": "false",
+#          "public": "true",
+#          "prevent_vul": "false"
+#        }
+#      }' -k --verbose
